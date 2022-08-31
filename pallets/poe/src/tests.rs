c5fcdbcf20e5f7e90c9edbac6b2e87c902e2b99e
Daniel Sun <dannysun85@gmail.com>
2022-08-31 20:23:29 +0800

    add test

--- pallets/poe/src/tests.rs
+++ pallets/poe/src/tests.rs
@@ -5,7 +5,7 @@ use super::*;
 #[test]
 fn create_claim_works() {
     new_test_ext().execute_with(|| {
-        let claim = vec![1, 2,];
+        let claim = vec![1, 2];
         assert_ok!(PoeModule::create_claim(Origin::signed(1), claim.clone()));
 
         let bounded_claim = BoundedVec::<u8,<Test as Config>::MaxClaimLength>::try_from(claim.clone()).unwrap();
@@ -18,10 +18,32 @@ fn create_claim_works() {
 #[test]
 fn create_claim_failed_when_claim_already_exists() {
     new_test_ext().execute_with(|| {
-        let claim = vec![1, 2,];
+        let claim = vec![1, 2];
         let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
 
         assert_noop!(PoeModule::create_claim(Origin::signed(1), claim.clone()), Error::<Test>::ProofAlreadyExist);
+    });
+}
+
+#[test]
+fn revoke_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![1, 2];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        let bounded_claim = BoundedVec::<u8,<Test as Config>::MaxClaimLength>::try_from(claim.clone()).unwrap();
+        assert_ok!(PoeModule::revoke_claim(Origin::signed(1), claim.clone()));
+        assert_eq!(Proofs::<Test>::get(&bounded_claim), None);
+    });
+}
+
+#[test]
+fn transfer_claim_works() {
+    new_test_ext().execute_with(|| {
+        let claim = vec![1, 2];
+        let _ = PoeModule::create_claim(Origin::signed(1), claim.clone());
+        let bounded_claim = BoundedVec::<u8,<Test as Config>::MaxClaimLength>::try_from(claim.clone()).unwrap();
+        assert_ok!(PoeModule::transfer_claim(Origin::signed(1), claim.clone(), 2u64));
+        assert_eq!(Proofs::<Test>::get(&bounded_claim), Some((2u64,frame_system::Pallet::<Test>::block_number())));
     });
 }
 
